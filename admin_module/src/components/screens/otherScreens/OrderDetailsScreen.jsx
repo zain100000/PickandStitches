11303bfb224b9684ddb1f3a590942794ae6e8a8a
zain100000 <muhammadzainulabideen292@gmail.com>
2024-10-31 13:44:10 +0500

    modify admin module

--- admin_module/src/components/screens/otherScreens/OrderDetailsScreen.jsx
+++ admin_module/src/components/screens/otherScreens/OrderDetailsScreen.jsx
@@ -114,8 +114,8 @@ const OrderDetailsScreen = () => {
                         <td>{order.puncha}</td>
                       </tr>
                       <tr>
-                        <th>Top Stitch</th>
-                        <td>{order.Top_double_stitch}</td>
+                        <th>Top</th>
+                        <td>{order.top}</td>
                       </tr>
                       <tr>
                         <th>Embroidery</th>
